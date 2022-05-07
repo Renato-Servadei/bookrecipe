225e5707c16c02283411984cb84265235a816521
Rober <boblawyer1@gmail.com>
2022-05-07 16:07:29 -0300

    creados los componentes y la vista general

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.sass']
 })
 export class RecipeListComponent implements OnInit {
+  recipes : Recipe[] = [
+    new Recipe("Locro de Zapallo", "Andean pumpkin, potato and black mint stew", "https://www.perudelights.com/wp-content/uploads/2011/12/locrfo-R-1024x690.jpg") 
+
+  ] ;
 
   constructor() { }
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../Shared/Ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.sass']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Mondongo', 200),
+    new Ingredient('Chorizo', 200),
+  ];
 
   constructor() { }
 
